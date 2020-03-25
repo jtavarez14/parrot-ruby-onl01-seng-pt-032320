990d71fe7ecf07b66bd8fd876f5e1e8680027cc2
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-03-25 18:46:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,6 +2,13 @@
 # returns the phrase
 
 def parrot(phrase="Squawk!")
+<<<<<<< HEAD
   puts phrase
   return phrase 
 end
+=======
+  phrase = "Squawk!"
+  puts #{phrase}
+  return "Squawk!"
+end
+>>>>>>> 6588183f38087f7261a1c778953da8082f943f2d
